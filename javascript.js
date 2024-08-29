2dbd24456c1f2876a71cddf43400c9fb747184ce
ChrisCode23 <178373270+ChrisCode23@users.noreply.github.com>
2024-08-29 16:32:10 +0200

    Insert getComputerChoice function in Javascript.js
    The function randomly selects either "Rock",
    "Scissors" or "Paper" and it will be used in the
    game by the computer

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+//Store an array global variable "gameChoices" which includes "Rock", "Scissor" and "Paper"
+let gameChoices = ["Rock", "Paper", "Scissor"];
+
+//Create a function which randomly returns "Rock", "Scissor" or "Paper" array element for the computer
+function getComputerChoice(){
+    return gameChoices[Math.floor(Math.random() * gameChoices.length)];
+}
+console.log(getComputerChoice());
+
+//Create a function which allows user to select "Rock", "Scissor" or "Paper" via a prompt
