b8b3384304bc5b2a72719f3d0f61ac9195a770c6
ChrisCode23 <178373270+ChrisCode23@users.noreply.github.com>
2024-08-29 16:35:37 +0200

    Fix "scissor" typo
    Fixes a typo error where scissors was misspelled
    as scissor

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-//Store an array global variable "gameChoices" which includes "Rock", "Scissor" and "Paper"
-let gameChoices = ["Rock", "Paper", "Scissor"];
+//Store an array global variable "gameChoices" which includes "Rock", "Scissors" and "Paper"
+let gameChoices = ["Rock", "Paper", "Scissors"];
 
-//Create a function which randomly returns "Rock", "Scissor" or "Paper" array element for the computer
+//Create a function which randomly returns "Rock", "Scissors" or "Paper" array element for the computer
 function getComputerChoice(){
     return gameChoices[Math.floor(Math.random() * gameChoices.length)];
 }
 console.log(getComputerChoice());
 
-//Create a function which allows user to select "Rock", "Scissor" or "Paper" via a prompt
+//Create a function which allows user to select "Rock", "Scissors" or "Paper" via a prompt
