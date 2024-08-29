21a1a8bd68e307744173fd57de124e6724dbf677
ChrisCode23 <178373270+ChrisCode23@users.noreply.github.com>
2024-08-29 17:47:22 +0200

    Add getUserChoice function
    Returns the choice made by the user and checks if
    it matches with the choices available specified
    within the "gameChoices" variable. If it doesn't
    match, returns an error message

--- javascript.js
+++ javascript.js
@@ -5,6 +5,23 @@ let gameChoices = ["Rock", "Paper", "Scissors"];
 function getComputerChoice(){
     return gameChoices[Math.floor(Math.random() * gameChoices.length)];
 }
-console.log(getComputerChoice());
 
-//Create a function which allows user to select "Rock", "Scissors" or "Paper" via a prompt
+//Create a function which allows user to select "Rock", "Scissors" or "Paper" via a prompt, then store it into "userChoice" and return the variable
+function getUserChoice(){
+    let userChoice = prompt("What do you choose: Rock, Scissors or Paper?", " ");
+
+    //Loops through all array elements in "gameChoices", and if "userChoice" equals one of the array elements, returns "userChoice"
+    for(let i = 0; i < gameChoices.length; i++)
+        
+        //Checks if the data inserted by the user and stored into "userChoice" matches one of the choices in "gameChoices"
+        {
+        if(gameChoices[i] === userChoice   
+            || gameChoices[i].toUpperCase() === userChoice    //Makes the data inserted by user case insensitive, same as next line
+            || gameChoices[i].toLowerCase() === userChoice){
+                return gameChoices[i];    //Returns the respective choice (i.e. "Rock" will return "Rock", "ROCK" will return "Rock")
+        } else {
+            alert ("Incorrect Choice!");
+        }
+    }
+}
+console.log(getUserChoice());
