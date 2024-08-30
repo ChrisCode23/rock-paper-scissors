55b0a3b3411b3b77c32f572b40aa629dba4cdb2d
ChrisCode23 <178373270+ChrisCode23@users.noreply.github.com>
2024-08-30 14:05:21 +0200

    Make userChoice case-insensitive
    Replaced the old way to make userChoice
    case-insensitive as it was inefficient.
    Now the prompt message the user inputs will
    always lowercase, as well as the array elements
    in the for loop so that they actually match to
    "userChoice". This implies that even if the user
    types "SciSsors", it will be lowercased and turn
    to "scissors", which will be compared to the array
    element "scissors", and ultimately if the
    comparison is true, will return "Scissors"

--- javascript.js
+++ javascript.js
@@ -10,18 +10,16 @@ function getComputerChoice(){
     return gameChoices[Math.floor(Math.random() * gameChoices.length)];
 }
 
-//Create a function which allows user to select "Rock", "Scissors" or "Paper" via a prompt, then store it into "userChoice" and return the variable
+//Create a function which allows user to select "Rock", "Scissors" or "Paper" via a prompt, then store it into "userChoice"
 function getUserChoice(){
-    let userChoice = prompt("What do you choose: Rock, Scissors or Paper?", " ");
+    let userChoice = prompt("What do you choose: Rock, Scissors or Paper?", " ").toLowerCase(); //Make the choice case-insensitive with "toLowerCase"
     //Create boolean variable "isFound" which determines if one of the values of "gameChoices" has been found
     let isFound = false;
 
     //Loop through all array elements in "gameChoices"
     for(let i = 0; i < gameChoices.length; i++){
         //Check if the data inserted by the user and stored into "userChoice" matches one of the array elements in "gameChoices"
-        if(gameChoices[i] === userChoice   
-            || gameChoices[i].toUpperCase() === userChoice  //Make the data inserted by user case insensitive, same as next line
-            || gameChoices[i].toLowerCase() === userChoice){
+        if(gameChoices[i].toLowerCase() === userChoice){  //Use "toLowerCase" to make the array elements lowercase, so they can match "userChoice" 
                 isFound = true; //Set "isFound" to true as one of the values was found
                 return gameChoices[i];  //Return the respective choice (i.e. "Rock" will return "Rock", "ROCK" will return "Rock")
         } 
